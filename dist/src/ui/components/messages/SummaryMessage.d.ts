1fef73412621374d11a8308e9a5fcc195bb42f0b
HitotokiKitsune <128005574+HitotokiKitsune@users.noreply.github.com>
2025-09-18 21:14:38 -0600

    Snapshot of changes from qwen3

---
+++ dist/src/ui/components/messages/SummaryMessage.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2025 Qwen
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import React from 'react';
+import { SummaryProps } from '../../types.js';
+export interface SummaryDisplayProps {
+    summary: SummaryProps;
+}
+export declare const SummaryMessage: React.FC<SummaryDisplayProps>;
